7bfbdcb7950e680393eab3fa54e534025f91118d
Magno <magnoapt@gmail.com>
2024-04-25 02:00:41 -0300

    desafio finalizado

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner bc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência!: ");
+        agencia = bc.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta!: ");
+        numero = bc.nextInt();
+
+        bc.nextLine();
+
+        System.out.println("Por favor, digite o seu Nome!: ");
+        nomeCliente = bc.nextLine();
+
+        System.out.println("Por favor digite o saldo Disponível!: ");
+        saldo = bc.nextDouble();
+
+        System.out.println("**************************************");
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque." );
+        
+        bc.close();
     }
 }
