1704df3e173d771c8c6ccd7439070c77fb39018a
Magno <magnoapt@gmail.com>
2024-05-01 18:27:16 -0300

    Iniciando o desafio

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmentro");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmentro");
+        int paramentroDois = terminal.nextInt();
+
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+
+
+
+
+
+
+        terminal.close();
+    }
+}
